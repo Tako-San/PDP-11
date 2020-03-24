d69f69829a8cb857b745f010d0860484aefa2456
Tako-San <dxe13@mail.ru>
2020-03-24 22:25:20 +0300

    Tests in their own dir. DBG INDENT added.

--- tests/tst.c
+++ tests/tst.c
@@ -1,32 +1,33 @@
-#include "pdp.h"
-#include "tst.h"
+#include "../headers/pdp.h"
+#include "../headers/tst.h"
+#include "../headers/cmd.h"
 
 void test_rw_bw()
 {$;
     //test1
-    head("byte w/r");
-    byte b1 = 0x0b;
+    head("Byte w/r");
+    Byte b1 = 0x0b;
     b_write(2, b1);
     printf("\n%02hhx = %02hhx\n", b_read(2), b1);
 
     //test2
-    head("word w/r");
-    word w1 = 0xACAB;
+    head("Word w/r");
+    Word w1 = 0xACAB;
     w_write(4, w1);
     printf("\n%02hx = %02hx\n", w_read(4), w1);
 
     //test3
-    head("byte write word read");
-    byte b2 = 0xcd;
-    byte b3 = 0xab;
+    head("Byte write Word read");
+    Byte b2 = 0xcd;
+    Byte b3 = 0xab;
 
     b_write(6, b2);
     b_write(7, b3);
     printf("\n%04hx = %02hhx%02hhx\n", w_read(6), b3, b2);
 
     //test3
-    head("word w/r");
-    word w2 = 0x0a0b;
+    head("Word w/r");
+    Word w2 = 0x0a0b;
     w_write(8, w2);
     printf("\n%04hx = %04hx\n", w_read(8), w2);
 
@@ -43,10 +44,17 @@ void test_argv(int argc, const char ** argv)
         printf("%s\n", argv[i]);
 $$;}
 
-void test3(const char * filename)
+void test_file(const char * filename)
 {$;
     load_file(filename);
     mem_dump(64, 2);
+$$;}
+
+void test_run(const char * filename)
+{$;
+   load_file(filename);
+   //mem_dump(2, 4);
+   run();
 $$;}
 
 void head(const char * msg)
@@ -63,7 +71,11 @@ void DBG(const char * file, const char * func, int line, int relay)
     if(relay == 1)
     {
         lvl++;
-        printf("%s [%02d]%*s >>> %s\n", file, line, 4 * lvl, "", func);
+        printf("%11s [%02d]%*s >>> %s\n", file, line, 4 * lvl, "", func);
+    }
+    else if(relay == 2)
+    {
+        printf("%*s", 4*lvl + 21, "");
     }
     else
     {
