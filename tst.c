8e424097438f6e268d0ffe9db26cb7a6a9a362bf
Tako-San <dxe13@mail.ru>
2020-03-26 00:30:28 +0300

    tst.c replaced

--- tst.c
+++ tst.c
@@ -1,6 +1,7 @@
 #include "../headers/pdp.h"
 #include "../headers/tst.h"
 #include "../headers/cmd.h"
+#include "../headers/dbg.h"
 
 void test_rw_bw()
 {$;
@@ -8,13 +9,13 @@ void test_rw_bw()
     head("Byte w/r");
     Byte b1 = 0x0b;
     b_write(2, b1);
-    printf("\n%02hhx = %02hhx\n", b_read(2), b1);
+    trace("\n%02hhx = %02hhx\n", b_read(2), b1);
 
     //test2
     head("Word w/r");
     Word w1 = 0xACAB;
     w_write(4, w1);
-    printf("\n%02hx = %02hx\n", w_read(4), w1);
+    trace("\n%02hx = %02hx\n", w_read(4), w1);
 
     //test3
     head("Byte write Word read");
@@ -23,25 +24,25 @@ void test_rw_bw()
 
     b_write(6, b2);
     b_write(7, b3);
-    printf("\n%04hx = %02hhx%02hhx\n", w_read(6), b3, b2);
+    trace("\n%04hx = %02hhx%02hhx\n", w_read(6), b3, b2);
 
     //test3
     head("Word w/r");
     Word w2 = 0x0a0b;
     w_write(8, w2);
-    printf("\n%04hx = %04hx\n", w_read(8), w2);
+    trace("\n%04hx = %04hx\n", w_read(8), w2);
 
     //test4
     head("little/big endian");
     uint16_t x = 1; /* 0x0001 */
-    printf("%s\n", *((uint8_t *) &x) == 0 ? "big-endian" : "little-endian");
+    trace("%s\n", *((uint8_t *) &x) == 0 ? "big-endian" : "little-endian");
 
 $$;}
 
 void test_argv(int argc, const char ** argv)
 {$;
     for(int i = 0; i <argc; i++)
-        printf("%s\n", argv[i]);
+        trace("%s\n", argv[i]);
 $$;}
 
 void test_file(const char * filename)
@@ -57,31 +58,9 @@ void test_run(const char * filename)
    run();
 $$;}
 
-
-
-
 void head(const char * msg)
 {
     static uint16_t counter;
     counter++;
-    //printf("\n-----------------------%s test%d------------------------\n", msg, counter);
-}
-
-void DBG(const char * file, const char * func, int line, int relay)
-{
-    static int lvl = 0;
-
-    if(relay == 1)
-    {
-        lvl++;
-        printf("%11s [%02d]%*s >>> %s\n", file, line, 4 * lvl, "", func);
-    }
-    else if(relay == 2)
-    {
-        printf("%*s", 4*lvl + 22, "");
-    }
-    else
-    {
-        lvl--;
-    }
+    //trace("\n-----------------------%s test%d------------------------\n", msg, counter);
 }
