2ea25ac409614068a56c919753dff2879061f1fb
Tako-San <dxe13@mail.ru>
2020-03-27 01:18:24 +0300

    Start changed

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "headers/tst.h"
 #include "headers/dbg.h"
+#include "headers/pdp.h"
+#include "headers/cmd.h"
 #include <string.h>
 
 int tr = 0;
@@ -16,14 +18,23 @@ int main(int argc, char * argv[])
         printf("\nUsage:\n\n");
         return 0;
     }
-
-    if(strcmp(argv[1], "-t") != 0 && strcmp(argv[1], "-T") != 0 )
-        test_run(argv[1]);
-    else
+    else if(argc == 2)
+    {
+        load_file(argv[1]);
+        run();
+    }
+    else if(strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "-T") == 0 )
     {
         tr = 1;
-        test_run(argv[2]);
+        load_file(argv[2]);
+        run();
     }
+/*    else
+    {
+        tr = 1;
+        load_file(argv[2]);
+        run();
+    }*/
 
 
 
